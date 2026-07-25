53a7c95456d2797e0ac6a616ad0f1aca8cb9e83b
agus <axsapari@gmail.com>
2026-07-25 16:21:00 +0700

    setup deploy ke github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/jurnal-guru-n9/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
